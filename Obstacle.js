f4ac440170e90eab25a966933e4e94e43e8bbe8d
LarsZauberer <wasser.ian@gmail.com>
2020-12-08 18:54:22 +0100

    Open Processing download

---
+++ Obstacle.js
@@ -0,0 +1,20 @@
+class Obstacle
+{
+constructor(x,y,w,h, world)
+	{
+		this.x = x+w/2;
+		this.y = y+h/2;
+		this.w = w;
+		this.h = h;
+		this.body = Bodies.rectangle(this.x,this.y,this.w,this.h, {isStatic: true});
+		World.add(world,this.body)
+		
+		this.ground = new Ground(this.x,this.y,this.w, this.h, world)
+	}
+	
+	update()
+	{
+		rect(this.x,this.y,this.w,this.h);
+		this.ground.update();
+	}
+}
