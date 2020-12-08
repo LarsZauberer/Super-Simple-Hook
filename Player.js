68e6619fc391d68271992b71e54e9bcfd6093a73
LarsZauberer <wasser.ian@gmail.com>
2020-12-08 19:16:22 +0100

    Player conventions fix

--- Player.js
+++ Player.js
@@ -19,34 +19,31 @@ class Player {
 	show() {
 		push();
 		translate(this.body.position.x, this.body.position.y);
-		rotate(this.body.angle)
+		rotate(this.body.angle);
 		rect(0, 0, 80, 80);
 		pop();
 	}
 
 
-
 	move() {
 		if (keyIsDown(65)) Body.applyForce(this.body, {
 			x: this.body.position.x,
-			y: this.body.position.y
+			y: this.body.position.y,
 		}, {
 			x: -0.01,
-			y: 0
+			y: 0,
 		});
 		if (keyIsDown(68)) Body.applyForce(this.body, {
 			x: this.body.position.x,
-			y: this.body.position.y
+			y: this.body.position.y,
 		}, {
 			x: 0.01,
-			y: 0
+			y: 0,
 		});
 	}
 
 
-
 	jump(ground) {
-		
 		if (this.canJump(ground)) Body.applyForce(this.body, {
 			x: this.body.position.x,
 			y: this.body.position.y
@@ -55,15 +52,12 @@ class Player {
 			y: -0.5
 		});
 	}
-	
 
-canJump(ground) {
-	var collision = Matter.SAT.collides(this.body, ground.body);
-	if (collision.collided) {
-		return true;
-		
-	}
-}
-	
 
+    canJump(ground) {
+        var collision = Matter.SAT.collides(this.body, ground.body);
+        if (collision.collided) {
+            return true;
+        }
+    }
 }
