a0bf8179a8d37efba2f0d3aa2377dda728eeb714
LarsZauberer <wasser.ian@gmail.com>
2020-12-09 09:40:11 +0100

    Ground Rewritten

--- Objects/Ground.js
+++ Objects/Ground.js
@@ -1,17 +1,5 @@
-class Ground {
-	constructor(x,y,w,h, world) {
-		this.h = 10
-
-		this.x = x
-		this.y = y-h/2+this.h/2
-		this.w = w-5;
-
-		this.body = Bodies.rectangle(this.x,this.y,this.w,this.h, {isStatic: true});
-		World.add(world,this.body);
-
-	}
-
-	update() {
+class Ground extends GameObject {
+	mesh() {
 		push();
 		fill(255,0,0);
 		rect(this.x,this.y,this.w,this.h);
