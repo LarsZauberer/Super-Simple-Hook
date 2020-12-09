e8483f9b4182f42fba61d886529fecc19596ea6a
LarsZauberer <wasser.ian@gmail.com>
2020-12-09 09:00:25 +0100

    Restructure

--- Ground.js
+++ Ground.js
@@ -1,21 +1,18 @@
-class Ground
-{
-constructor(x,y,w,h, world)
-	{
-	this.h = 10
-		
-	this.x = x
-	this.y = y-h/2+this.h/2
-	this.w = w-5;
-		
+class Ground {
+	constructor(x,y,w,h, world) {
+		this.h = 10
+
+		this.x = x
+		this.y = y-h/2+this.h/2
+		this.w = w-5;
+
 		this.body = Bodies.rectangle(this.x,this.y,this.w,this.h, {isStatic: true});
 		World.add(world,this.body);
-	
+
 	}
-	
-	update()
-	{
-	push();
+
+	update() {
+		push();
 		fill(255,0,0);
 		rect(this.x,this.y,this.w,this.h);
 		pop();
