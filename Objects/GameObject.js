7e3e2666fc2b835ba368b4819fed7e0a5f4d8d3f
LarsZauberer <wasser.ian@gmail.com>
2020-12-09 09:38:51 +0100

    GameObject Class finished

--- Objects/GameObject.js
+++ Objects/GameObject.js
@@ -1,20 +1,19 @@
 class GameObject {
-    constructor (world, x, y) {
+    constructor (world, x, y, sx, sy, static=false) {
         this.x = x;
         this.y = y;
 
-        this.size;
+        this.size = createVector(sx, sy);
+        this.body = Bodies.rectangle(x, y, sx, sy, {isStatic: static});
+        World.add(world, this.body)
     }
 
     update () {
-        // TODO: GameObject Update
+        // GameObject Update
+        this.mesh();
     }
 
     mesh() {
-        // Empty Overwrite Function
-    }
-
-    hitbox() {
         // Empty Overwrite Function
     }
 }
