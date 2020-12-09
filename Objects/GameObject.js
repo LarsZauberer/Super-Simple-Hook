ed0782597e46060f0b33645c6387ba24dbd46f28
LarsZauberer <wasser.ian@gmail.com>
2020-12-09 09:17:30 +0100

    GameObject Class Structure

---
+++ Objects/GameObject.js
@@ -0,0 +1,20 @@
+class GameObject {
+    constructor (world, x, y) {
+        this.x = x;
+        this.y = y;
+
+        this.size;
+    }
+
+    update () {
+        // TODO: GameObject Update
+    }
+
+    mesh() {
+        // Empty Overwrite Function
+    }
+
+    hitbox() {
+        // Empty Overwrite Function
+    }
+}
