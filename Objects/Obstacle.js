c237e109704e94ffb9b44e120bfe0069beb5c806
LarsZauberer <wasser.ian@gmail.com>
2020-12-11 21:37:50 +0100

    Commenting Ground + Obstacle

--- Objects/Obstacle.js
+++ Objects/Obstacle.js
@@ -1,15 +1,20 @@
 class Obstacle extends GameObject {
 	constructor(world, x, y, w, h) {
 		super(world, x, y, w, h, true);
-		this.ground = new Ground(world, x, y-h/2, w, 10, true);
+
+		// Create the ground for the object
+		let groundpos = y-h/2;
+		this.ground = new Ground(world, x, groundpos, w, 10, true);
 	}
 
 	update() {
+		// Update everything
 		super.update();
 		this.ground.update();
 	}
 
 	mesh() {
+		// Draw the Main Obstacle
 		fill(255, 255, 255)
 		rect(this.x, this.y, this.size.x, this.size.y);
 	}
