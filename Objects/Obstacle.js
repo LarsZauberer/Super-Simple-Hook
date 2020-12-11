8e83d857fc73451682be05dff7387488ca0260df
LarsZauberer <wasser.ian@gmail.com>
2020-12-11 21:44:45 +0100

    New Pos Calculation

--- Objects/Obstacle.js
+++ Objects/Obstacle.js
@@ -1,10 +1,14 @@
 class Obstacle extends GameObject {
 	constructor(world, x, y, w, h) {
+		// Position Calculation
+		x = x+w/2
+		y = y+h/2
+
 		super(world, x, y, w, h, true);
 
 		// Create the ground for the object
 		let groundpos = y-h/2;
-		this.ground = new Ground(world, x, groundpos, w, 10, true);
+		this.ground = new Ground(world, x, groundpos+5, w, 10, true);
 	}
 
 	update() {
