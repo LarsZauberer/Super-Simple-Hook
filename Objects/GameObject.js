c63bd1c6f09f8e532c4dc205fe026107ac2b1308
LarsZauberer <wasser.ian@gmail.com>
2020-12-14 19:21:12 +0100

    Code Refactoring

--- Objects/GameObject.js
+++ Objects/GameObject.js
@@ -1,9 +1,14 @@
 class GameObject {
     constructor(world, x, y, sx, sy, staticMesh=false) {
+        // Constructor
+        // Position
         this.x = x;
         this.y = y;
 
+        // Size
         this.size = createVector(sx, sy);
+
+        // Matter.js Body
         this.body = Bodies.rectangle(x, y, sx, sy, {isStatic: staticMesh});
         World.add(world, this.body)
     }
@@ -14,6 +19,7 @@ class GameObject {
         this.mesh();
         pop();
 
+        // Change the position to the calculated physics position
         this.x = this.body.position.x
         this.y = this.body.position.y
     }
--- Objects/Obstacle.js
+++ Objects/Obstacle.js
@@ -1,8 +1,8 @@
 class Obstacle extends GameObject {
 	constructor(world, x, y, w, h, gs=10) {
 		// Position Calculation
-		x = x+w/2
-		y = y+h/2
+		x = x+w/2;
+		y = y+h/2;
 
 		super(world, x, y, w, h, true);
 
