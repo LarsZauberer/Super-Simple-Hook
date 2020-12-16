527b6fbf3ebc09aa990a4d530b2173b8c75ecff9
LarsZauberer <wasser.ian@gmail.com>
2020-12-16 09:31:01 +0100

    Working Maploading

--- Objects/MapManager.js
+++ Objects/MapManager.js
@@ -1,42 +1,40 @@
 class MapManager {
     constructor(names) {
         this.mapNames = names;
-        this.maps = [];
-        this.loaded = false;
+        this.loaded = 0;
 
     this.load();
-    /*while (this.loaded == false) {
-        // Empty to wait until all maps are loaded
-    }*/
     }
 
     load() {
-        for (let index = 0; index < this.mapNames.length; index++) {
-            const element = this.mapNames[index];
-            let xml = new XMLHttpRequest();
-            xml.onreadystatechange = function() {
-                if (this.readyState == 4 && this.status == 200) {
-                    levelManager.maps.push(JSON.parse(this.responseText));
-                    console.log(levelManager);
-                    if (levelManager.maps.length == levelManager.mapNames.length) {
-                        levelManager.loaded = true;
+        let xml = new XMLHttpRequest();
+        xml.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                let map = JSON.parse(this.responseText);
+                /*for (let index = 0; index < obstacles.length; index++) {
+                    const element = obstacles[index];
+                    World.remove(world, element.body)
+                    if (element.target) {
+                        World.remove(world, element.target)
+                    }
+                    if (element.ground) {
+                        World.remove(world, element.ground)
+                    }
+                }*/
+                World.clear(world);
+                obstacles = [];
+                player = null;
+                for (let index = 0; index < map.mapData.length; index++) {
+                    const element = map.mapData[index];
+                    if (element.id == 1) {
+                        player = new Player(world, element.x, element.y, element.sx, element.sy);
+                    } else {
+                        obstacles.push(new objectRegistry[element.id](world, element.x, element.y, element.sx, element.sy))
                     }
                 }
-            };
-            xml.open("GET", element, true);
-            xml.send();
-        }
-    }
-
-    loadLevel(index, elements) {
-        let obst = []
-        let map = this.maps[index];
-        console.log(this.maps);
-
-        for (let index = 0; index < map.mapData.length; index++) {
-            const element = map.mapData[index];
-            obst.push(new elements[element.id](world, element.x, element.y, element.sx, element.sy, true))
-        }
-        return obst;
+            }
+        };
+        xml.open("GET", this.mapNames[this.loaded], true)
+        xml.send();
     }
 }
