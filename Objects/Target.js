7201819da21a3fc54852a9082be70c11aa350aff
RobRob1101 <robin.steiner00@gmail.com>
2020-12-16 08:33:37 +0100

    hook Stuff

--- Objects/Target.js
+++ Objects/Target.js
@@ -2,18 +2,10 @@ class Target extends GameObject {
     constructor(world, x, y) {
         // Constructor
         super(world, x, y, 10, 10, true)
-        this.canHook = false;
     }
 
     update() {
         super.update();
-
-        // Checking Mouse is in range
-        let sideCheck = this.x-50 <= mouseX+(-cam.x) && mouseX+(-cam.x) <= this.x+50
-        let heightCheck = this.y-50 <= mouseY && mouseY <= this.y+50
-
-        // CanHook?
-        this.canHook = sideCheck && heightCheck;
     }
 
     mesh() {
