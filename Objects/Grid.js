ab12f75b903cbb646953c5bae9f067aabb8950b8
LarsZauberer <wasser.ian@gmail.com>
2020-12-20 10:47:34 +0100

    Grid better

--- Objects/Grid.js
+++ Objects/Grid.js
@@ -1,18 +1,18 @@
 class Grid {
-    constructor(sx, sy) {
-        this.sx = sx;
-        this.sy = sy;
-
-        this.square = windowWidth/this.sy;
+    constructor(square) {
+        this.square = square;
+        this.sx = windowWidth/this.square;
+        this.sy = windowHeight/this.square;
     }
 
     update() {
         push();
+        rectMode(CORNER);
         fill(0, 0, 0, 0);
         stroke(0);
         for (let index = 0; index < this.sx; index++) {
             for (let e = 0; e < this.sy; e++) {
-                rect(this.square*index, this.square*e, this.square, this.square)
+                rect(this.square*index, this.square*e, this.square, this.square);
             }
         }
         pop();
