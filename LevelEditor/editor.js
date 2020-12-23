81d3782124ec79fe39d42b8a243eca3e540811b9
LarsZauberer <wasser.ian@gmail.com>
2020-12-23 11:50:30 +0100

    All Functions Fixed

--- LevelEditor/editor.js
+++ LevelEditor/editor.js
@@ -28,31 +28,36 @@ function loc(ele, posX, posY) {
     }
 }
 
-function scaleObj(ele, id, trans) {
+function scaleObj(ele, trans) {
     // Change Scaling
+
+    // Change MapData
+    for (let index = 0; index < mapData.mapData.length; index++) {
+        const element = mapData.mapData[index];
+        if ((Math.round((100/windowWidth*ele.x)-(100/windowWidth*ele.size.x)/2) == Math.round(element.x) && Math.round((50/windowHeight*ele.y)-(50/windowHeight*ele.size.y)/2) == Math.round(element.y)) || (Math.round(100/windowWidth*ele.x)) == Math.round(element.x) && Math.round(50/windowHeight*ele.y) == Math.round(element.y)) {
+            mapData.mapData[index] = {"x": 100/windowWidth*ele.x-(100/windowWidth*trans.x)/2,
+                                      "y": 50/windowHeight*ele.y-(50/windowHeight*trans.y)/2,
+                                      "sx": 100/windowWidth*trans.x,
+                                      "sy": 50/windowHeight*trans.y,
+                                      "id": element.id
+                                     }
+        }
+    }
 
     // Change Visuals
     // TODO: Target Update
 
     ele.size.x = trans.x
     ele.size.y = trans.y
-
-    // Change MapData
-    for (let index = 0; index < mapData.mapData.length; index++) {
-        const element = mapData.mapData[index];
-        if (element.id == id && 100/windowWidth*ele.x == element.x && 100/windowHeight*ele.y == element.y) {
-            mapData.mapData[index] = {"x": ele.x, "y": ele.y, "sx": 100/windowWidth*trans.x, "sy": 50/windowHeight*trans.y, "id": id}
-        }
-    }
 }
 
-function delFromMap(ele, id) {
+function delFromMap(ele) {
     // Delete Objects
 
     // Change MapData
     for (let index = 0; index < mapData.mapData.length; index++) {
         const element = mapData.mapData[index];
-        if (element.id == id && 100/windowWidth*ele.x == element.x && 100/windowHeight*ele.y == element.y) {
+        if ((Math.round((100/windowWidth*ele.x)-(100/windowWidth*ele.size.x)/2) == Math.round(element.x) && Math.round((50/windowHeight*ele.y)-(50/windowHeight*ele.size.y)/2) == Math.round(element.y)) || (Math.round(100/windowWidth*ele.x)) == Math.round(element.x) && Math.round(50/windowHeight*ele.y) == Math.round(element.y)) {
             mapData.mapData.splice(index, 1);
         }
     }
