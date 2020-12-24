208542889cb5c19333a288344b2cea4bcbed1140
LarsZauberer <wasser.ian@gmail.com>
2020-12-24 19:26:36 +0100

    Better Obstacle Drawing

--- LevelEditor/editor.js
+++ LevelEditor/editor.js
@@ -1,17 +1,22 @@
-function obstacleDraw(mx, my) {
+function obstacleDraw(pos1, pos2) {
     // Draw an Obstacle on the grid
     // Check if the obstacle already exists
+    let size = createVector(abs(pos1.x-pos2.x), abs(pos1.y-pos2.y));
+
     let found = false;
     for (let index = 0; index < obstacles.length; index++) {
         const element = obstacles[index];
-        if (element.x-element.size.x/2 == mx, element.y-element.size.y/2 == my) {
+        if (Math.round(element.x-element.size.x/2) == Math.round(pos1.x) && Math.round(element.y-element.size.y/2) == Math.round(pos1.y)) {
             found = true;
         }
     }
 
     // Create a new obstacle if it doesn't exist
     if (found == false) {
-        obstacles.push(new DevObstacle(world, mx, my, windowWidth/80, windowHeight/45));
-        mapData.obstacles.push({"x": 80/windowWidth*mx, "y": 45/windowHeight*my, "sx": 1, "sy": 1, "type": 0});
+        obstacles.push(new DevObstacle(world, pos1.x, pos1.y, size.x, size.y));
+        mapData.obstacles.push({"x": 80/windowWidth*pos1.x, "y": 45/windowHeight*pos1.y, "sx": 80/windowWidth*size.x, "sy": 45/windowHeight*size.y, "type": 0});
     }
+
+    mouseDown = null;
+    mouseUp = null;
 }
