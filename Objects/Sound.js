2dab329fcea631dd6cbefac976b70d038d8e8ccc
LarsZauberer <wasser.ian@gmail.com>
2020-12-25 10:40:10 +0100

    Sound Quick Fix

--- Objects/Sound.js
+++ Objects/Sound.js
@@ -2,6 +2,7 @@ class Sound {
     constructor(assets) {
         // Load all Songs
         this.sounds = [];
+        this.currentSound;
         for (let index = 0; index < assets.length; index++) {
             const element = assets[index];
             this.sounds.push(loadSound(element));
@@ -12,11 +13,17 @@ class Sound {
         // Calculate whole path
         name = "Assets/music/" + name;
 
+        if (this.currentSound) {
+            this.currentSound.stop();
+        }
+
         // Play File
         for (let index = 0; index < this.sounds.length; index++) {
             const element = this.sounds[index];
             if (element.file == name) {
-                element.play();
+                this.currentSound = element;
+                this.currentSound.play();
+                this.currentSound.loop();
             }
         }
     }
