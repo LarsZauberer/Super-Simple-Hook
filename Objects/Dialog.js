53a801dd3519140dcac352f60df66e17d0fe925c
LarsZauberer <wasser.ian@gmail.com>
2021-01-03 13:25:35 +0100

    Text Allignement

--- Objects/Dialog.js
+++ Objects/Dialog.js
@@ -8,15 +8,13 @@ class Dialog {
 
     show() {
         push()
+        rectMode(CORNER);
         image(dialogBack, 0, height/8*6, width, height/8*2);
         textSize(50);
         stroke(0)
         fill(0)
-        console.log(this.text[this.frame])
-        text(this.text[this.frame], 10, height/8*6+10, width-10, height-10);
-        console.log(this.changed)
-        console.log(millis())
-        if (keyIsPressed && this.changed+1000 < millis()) {
+        text(this.text[this.frame], width/16*1.5, height/16*13, width/16*13.5, height/16*14);
+        if (mouseIsPressed && this.changed+1000 < millis()) {
             this.frame++;
             if (this.frame > this.text.length-1) {
                 this.update = false;
