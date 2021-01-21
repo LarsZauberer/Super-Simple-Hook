7f4204a0aa20b4965fbb15edd66113c4b5bc43f5
LarsZauberer <wasser.ian@gmail.com>
2021-01-21 18:27:58 +0100

    Unstatic Rect refactored

--- Objects/HookFramework/UnstaticRect.js
+++ Objects/HookFramework/UnstaticRect.js
@@ -1,16 +1,21 @@
 class UnstaticRect extends GameObject {
     constructor(world, x, y, w, h, restitution) {
-        //unstatic
+        // Unstatic Rectangle
         super(world, x, y, w, h, false);
+
+        // Remove preconstructed Gameobject body
         World.remove(world, this.body)
+
+        // Create new body
         this.body = Bodies.rectangle(x, y, w, h, {
             chamfer: 5
         })
         World.add(world, this.body)
 
+        // Change restitution
         this.body.restitution = restitution
-        this.body.friction = 0.7;
 
+        // Set mass
         Body.setMass(this.body, 3);
         this.body.friction = 0.1;
         this.startPos = createVector(this.x, this.y);
@@ -18,18 +23,15 @@ class UnstaticRect extends GameObject {
 
 
     mesh() {
+        // Unstatic rect image
         translate(this.body.position.x, this.body.position.y)
         rotate(degrees(this.body.angle))
         image(unstaticImg, -this.size.x / 2, -this.size.y / 2, this.size.x, this.size.y)
 
+        // Debug mesh
         if (debug) {
             fill(0, 0, 100)
             rect(0, 0, this.size.x, this.size.y);
         }
-
-
     }
-
-
-
 }
