27693cc3ce7a6bb45e344be4ce84c7cb792df472
LarsZauberer <wasser.ian@gmail.com>
2021-02-10 15:56:24 +0100

    Pause on Console

---
+++ Assets/Scripts/HUD/ConsolePause.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConsolePause : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("escape")) {
+            switch(Time.timeScale) {
+                case 0:
+                    Time.timeScale = 1;
+                    break;
+                case 1:
+                    Time.timeScale = 0;
+                    break;
+            }
+
+            Debug.Log("Time-Scale now at: " + Time.timeScale);
+        }
+    }
+}
