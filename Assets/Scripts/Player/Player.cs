daac4aae4fed0433f1a275484d9d58e77979a477
LarsZauberer <wasser.ian@gmail.com>
2021-02-10 17:34:18 +0100

    Rename Player Script

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+public class Player : MonoBehaviour
 {
     public CharacterController2D controller;
     public float runSpeed = 40f;
