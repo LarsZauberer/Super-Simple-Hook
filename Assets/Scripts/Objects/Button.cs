79590b329b37e98b6a56cb42ed6ce68d645e9b7c
LarsZauberer <wasser.ian@gmail.com>
2021-02-10 17:41:25 +0100

    Button Script

---
+++ Assets/Scripts/Objects/Button.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour
+{
+    public bool isActive = false;
+    // TODO: Sprite Component
+
+    void FixedUpdate() {
+        // TODO: Change Sprite
+        if (isActive) {
+
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D col) {
+        isActive = true;
+    }
+
+    void OnCollisionExit2D(Collision2D col) {
+        isActive = false;
+    }
+}
