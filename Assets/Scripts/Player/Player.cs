60bf259aaf10e4d161bc83025a02ad7dd06c2299
LarsZauberer <wasser.ian@gmail.com>
2021-02-10 20:06:29 +0100

    X-Axis Cam Following

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -6,10 +6,15 @@ public class Player : MonoBehaviour
 {
     public CharacterController2D controller;
     public float runSpeed = 40f;
+    public GameObject camObj;
+    Vector2 camStartPos;
     float horizontalMove = 0f;
     bool jump = false;
 
-    // Update is called once per frame
+    void Start() {
+        camStartPos = camObj.transform.position;
+    }
+
     void Update()
     {
         horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
@@ -22,5 +27,13 @@ public class Player : MonoBehaviour
     void FixedUpdate() {
         controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
         jump = false;
+
+        camFollow();
+    }
+
+    void camFollow() {
+        if (camStartPos.x < transform.position.x) {
+            camObj.transform.position = new Vector3(transform.position.x, camObj.transform.position.y, -10);
+        }
     }
 }
