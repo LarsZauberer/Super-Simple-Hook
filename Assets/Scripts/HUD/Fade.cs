b6fdad80ae3387aa4c0fc94f2e8d368db3584919
LarsZauberer <wasser.ian@gmail.com>
2021-02-17 13:55:45 +0100

    Loading Fade

---
+++ Assets/Scripts/HUD/Fade.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Fade : MonoBehaviour
+{
+    private Image img;
+    public bool isFading = false;
+    public float fadingSpeed = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        img = GetComponent<Image>();
+        var tempCol = img.color;
+        tempCol.a = 0f;
+        img.color = tempCol;
+    }
+
+    void Update() {
+        if (isFading) {
+            var tempCol = img.color;
+            tempCol.a = tempCol.a + fadingSpeed * Time.deltaTime;
+            img.color = tempCol;
+            if (img.color.a >= 1) {
+                isFading = false;
+            }
+        }
+    }
+}
--- Assets/Scripts/Objects/LevelLoading.cs
+++ Assets/Scripts/Objects/LevelLoading.cs
@@ -6,8 +6,27 @@ using UnityEngine.SceneManagement;
 public class LevelLoading : MonoBehaviour
 {
     public string nextLevel;
+
     private void OnTriggerEnter2D(Collider2D other) {
-        if (other.gameObject.tag == "Player")
-        SceneManager.LoadScene(nextLevel);
+        // Fading
+        if (other.gameObject.tag == "Player") {
+            GameObject fadeObj = GameObject.Find("Fade");
+            Fade fading = fadeObj.GetComponent<Fade>();
+            fading.isFading = true;
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D other) {
+        // Load Next Level
+        if (other.gameObject.tag == "Player") {
+            GameObject fadeObj = GameObject.Find("Fade");
+            Fade fading = fadeObj.GetComponent<Fade>();
+
+            // Load Level
+            if (fading.isFading == false)
+            SceneManager.LoadScene(nextLevel);
+
+            
+        }
     }
 }
