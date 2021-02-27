9257672c320d07b4be9adcceb4201f20a37a23ae
LarsZauberer <wasser.ian@gmail.com>
2021-02-27 11:41:04 +0100

    Hook Single Object Behavior

---
+++ Assets/Scripts/Objects/HookBehavior.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HookBehavior : MonoBehaviour
+{
+    public float shootingSpeed = 20f;
+    public Vector2 dir;
+    private Rigidbody2D r;
+    // Start is called before the first frame update
+    void Start()
+    {
+        r = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        r.AddForce(dir*Time.fixedDeltaTime*shootingSpeed, ForceMode2D.Impulse);
+    }
+}
