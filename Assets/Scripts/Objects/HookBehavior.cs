d24c3ee5f7bce8f76fd85aa02458c43d450e41b4
LarsZauberer <wasser.ian@gmail.com>
2021-03-15 18:14:18 +0100

    Hook not working

--- Assets/Scripts/Objects/HookBehavior.cs
+++ Assets/Scripts/Objects/HookBehavior.cs
@@ -8,43 +8,54 @@ public class HookBehavior : MonoBehaviour
     public Vector2 dir;
     private Rigidbody2D r;
     public bool Hooked = false;
+    public GameObject hookedToObj;
     public float pullSpeed = 100f;
     public float xPower = 50f, yPower = 50f;
+    public GameObject pullObject;
+    public bool allowedToPull = false;
     // Start is called before the first frame update
     void Start()
     {
         r = GetComponent<Rigidbody2D>();
+        pullObject = GameObject.FindGameObjectWithTag("Player");
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
         if (!Hooked) {
             r.AddForce(dir*Time.fixedDeltaTime*shootingSpeed, ForceMode2D.Impulse);
         } else {
-            GameObject player = GameObject.FindGameObjectWithTag("Player");
             Rigidbody2D rb = player.GetComponent<Rigidbody2D>();
-            // rb.gravityScale = 0f;
 
-            MovePlayer();
+            if (!player.GetComponent<Hook>().DoubleMech && allowedToPull) {
+                MoveObj();
+            }
+        }
+
+        if (player.GetComponent<Hook>().bothHooked()) {
+            MoveObj();
         }
     }
 
     private void OnTriggerEnter2D(Collider2D other) {
-        if (other.tag == "Targets") {
+        if (other.tag == "Targets" || other.tag == "Unstatic") {
+            hookedToObj = other.gameObject;
             Hooked = true;
             r.velocity = new Vector2(0, 0);
         } else if (other.tag != "Player" && other.tag != "MainCamera") {
-            Destroy(gameObject);
+            GameObject player = GameObject.FindGameObjectWithTag("Player");
+            Hook h = player.GetComponent<Hook>();
+            h.deleteHook();
         }
     }
 
-    private void MovePlayer() {
-        GameObject player = GameObject.FindGameObjectWithTag("Player");
-        Player script = player.GetComponent<Player>();
-        Rigidbody2D rb = player.GetComponent<Rigidbody2D>();
+    private void MoveObj() {
+        Debug.Log(pullObject);
+        Rigidbody2D rb = pullObject.GetComponent<Rigidbody2D>();
         
-        Vector2 forceVector = CalculateDirection(transform.position, player.transform.position);
+        Vector2 forceVector = CalculateDirection(transform.position, pullObject.transform.position);
 
         Debug.Log(forceVector*Time.fixedDeltaTime*pullSpeed);
         rb.AddForce(forceVector*Time.fixedDeltaTime*pullSpeed, ForceMode2D.Impulse);
--- Assets/Scripts/Player/Hook.cs
+++ Assets/Scripts/Player/Hook.cs
@@ -6,25 +6,51 @@ using QFSW.QC;
 public class Hook : MonoBehaviour
 {
     private Vector3 mousePoint;
-    public GameObject hook;
-    public GameObject obj;
+    public GameObject hookPrefab;
+    public GameObject hook1;
+    public GameObject hook2;
+    public bool DoubleMech = false;
 
     void Update()
     {
         if (Input.GetButtonDown("Fire1")) {
             Shoot();
+        } else if (Input.GetButtonUp("Fire1") && hook1) {
+            HookBehavior hb = hook1.GetComponent<HookBehavior>();
+            if (hb.Hooked) {
+                DoubleMech = true;
+                ShootSecondHook();
+            } else {
+                hb.allowedToPull = true;
+            }
+        }
+
+        if (bothHooked()) {
+            setNewObj();
         }
     }
 
     public void Shoot() {
-        if (obj) {
+        DoubleMech = false;
+        if (hook1) {
             deleteHook();
         }
         mousePoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        obj = Instantiate(hook) as GameObject;
-        obj.transform.position = transform.position;
-        HookBehavior hb = obj.GetComponent<HookBehavior>();
+        hook1 = Instantiate(hookPrefab) as GameObject;
+        hook1.transform.position = transform.position;
+        HookBehavior hb = hook1.GetComponent<HookBehavior>();
         hb.dir = CalculateDirection(mousePoint, transform.position);
+    }
+
+    public void ShootSecondHook() {
+        HookBehavior hb1 = hook1.GetComponent<HookBehavior>();
+
+        // Create second hook
+        mousePoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        hook2 = Instantiate(hookPrefab) as GameObject;
+        hook2.transform.position = transform.position;
+        HookBehavior hb2 = hook2.GetComponent<HookBehavior>();
+        hb2.dir = CalculateDirection(mousePoint, transform.position);
     }
 
     public Vector2 CalculateDirection(Vector3 mouse, Vector3 player) {
@@ -41,12 +67,33 @@ public class Hook : MonoBehaviour
     public void deleteHook() {
         Rigidbody2D playerRb = GameObject.FindGameObjectWithTag("Player").GetComponent<Rigidbody2D>();
         playerRb.gravityScale = 3f;
-        Destroy(obj);
+        Destroy(hook1);
+        Destroy(hook2);
     }
 
     private void OnCollisionEnter2D(Collision2D other) {
         if (other.gameObject.tag == "Targets") {
             deleteHook();
         }
+    }
+
+    public bool bothHooked() {
+        if (!DoubleMech) {
+            return false;
+        }
+
+        HookBehavior hb1 = hook1.GetComponent<HookBehavior>();
+        HookBehavior hb2 = hook2.GetComponent<HookBehavior>();
+
+        return hb1.Hooked && hb2.Hooked;
+    }
+
+    public void setNewObj() {
+        // Set each others
+        HookBehavior hb1 = hook1.GetComponent<HookBehavior>();
+        HookBehavior hb2 = hook2.GetComponent<HookBehavior>();
+
+        hb1.pullObject = hb2.hookedToObj;
+        hb2.pullObject = hb1.hookedToObj;
     }
 }
